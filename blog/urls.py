e93f64e20839a9eb42006ad147fb4e1a7bdfbc9e
pybm <139861902+pybm@users.noreply.github.com>
2024-01-10 13:18:43 +0200

    Delete blog directory

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('welcome.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('notes.urls')),
-]
