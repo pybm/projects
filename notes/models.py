39907bef278078a8f0b1c11accac1c9180b77118
pybm <139861902+pybm@users.noreply.github.com>
2024-01-10 13:19:18 +0200

    Delete notes directory

--- notes/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Notes(models.Model):
-  name = models.CharField(max_length=255)
-  details = models.CharField(max_length=255)
-  link = models.URLField(max_length=255)
