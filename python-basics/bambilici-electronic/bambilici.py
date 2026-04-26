6ea2a65f08374df71ce32b51f0d0daf98d891d04
pybm <139861902+pybm@users.noreply.github.com>
2026-04-26 18:04:15 +0300

    Update bambilici.py

--- python-basics/bambilici-electronic/bambilici.py
+++ python-basics/bambilici-electronic/bambilici.py
@@ -1,1 +1,7 @@
+def bambilici_electronic():
+    x_num = int(input("Alege un numar: "))
+    y_num = x_num + 1
+    txt = "Am ales {}, tu {}. Ai pierdut!"
+    print(txt.format(y_num, x_num))
 
+bambilici_electronic()
